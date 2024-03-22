04796f45101f742675b0850b68fa0995b1426cf8
auritiwdesign <juancamilo.auriti@gmail.com>
2024-03-22 07:00:29 +0100

    aggiornato store.ts e creato il file hooks.ts
    ### Spiegazione dettagliata:
    
    **1\. Importazioni:**
    
    *   configureStore da @reduxjs/toolkit: funzione per creare un nuovo store Redux.
    
    *   cartReducer, loadingReducer, productsReducer: importano i reducer per le funzionalità di carrello, caricamento e prodotti.
    
    **2\. Creazione dello store:**
    
    *   La funzione configureStore viene utilizzata per creare lo store Redux.
    
    *   L'oggetto di configurazione passato alla funzione specifica:
    
        *   reducer: un oggetto che mappa i nomi dei reducer alle funzioni reducer specifiche.
    
        *   devTools: abilita gli strumenti di sviluppo Redux se l'ambiente non è di produzione.
    
    **3\. Reducer:**
    
    *   Vengono importati e utilizzati tre reducer:
    
        *   cartReducer: gestisce lo stato del carrello, come l'aggiunta e la rimozione di prodotti.
    
        *   productsReducer: gestisce lo stato dei prodotti, come il caricamento e l'aggiornamento dei dati.
    
        *   loadingReducer: gestisce lo stato di caricamento, mostrando un indicatore di caricamento quando necessario.
    
    **4\. Tipi per lo stato e l'azione:**
    
    *   Vengono esportati due tipi:
    
        *   RootState: rappresenta il tipo dello stato dell'applicazioe. Questo tipo è l'unione dei tipi di stato di tutti i reducer.
    
        *   AppDispatch: rappresenta il tipo della funzione di dispatch dell'azione. Questo tipo permette di inviare azioni allo store in modo sicuro e tipizzato.
    
    **Note:**
    
    *   L'utilizzo di Redux Toolkit facilita la creazione di un boilerplate Redux con una configurazione minima.
    
    *   I tipi per lo stato e l'azione aiutano a migliorare la leggibilità e la sicurezza del codice.
    
    *   I reducer sono organizzati in base a funzionalità per una migliore modularità e manutenzione.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+// Importa i reducer
+import cartReducer from "./features/cartSlice";
+import loadingReducer from "./features/loadingSlice";
+import productsReducer from "./features/productsSlice";
+
 // Esporta lo store Redux creato con configureStore
 export const store = configureStore({
   // Riduttore: definisce lo stato iniziale e come lo stato cambia in risposta alle azioni
-  reducer: {},
+  reducer: {
+    cart: cartReducer, // Gestisce lo stato del carrello
+    products: productsReducer, // Gestisce lo stato dei prodotti
+    loading: loadingReducer, // Gestisce lo stato di caricamento
+  },
 
   // DevTools: abilita gli strumenti di sviluppo Redux in ambiente di sviluppo
   devTools: process.env.NODE_ENV !== "production",
