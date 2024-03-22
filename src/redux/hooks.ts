0236f9b6571d32b72278fc3bd2577e55e6efbd17
auritiwdesign <juancamilo.auriti@gmail.com>
2024-03-22 07:01:23 +0100

    aggiunta commenti in hooks.ts
    ### Spiegazione dettagliata:
    
    **1\. Importazioni:**
    
    *   useDispatch, useSelector, TypedUseSelectorHook: importate da react-redux.
    
    *   AppDispatch, RootState: importate dal file store.ts (si presume).
    
    **2\. useAppDispatch Hook:**
    
    *   Questo hook personalizzato è una wrapper attorno a useDispatch di react-redux.
    
    *   Restituisce la funzione dispatch con il tipo corretto AppDispatch.
    
    *   Il tipo AppDispatch garantisce che le azioni dispatchate siano compatibili con lo store Redux dell'applicazione.
    
    **3\. useAppSelector Hook:**
    
    *   Questo hook personalizzato è una wrapper attorno a useSelector di react-redux.
    
    *   Assegna il tipo generico TypedUseSelectorHook a useAppSelector.
    
    *   Il tipo generico RootState specifica il tipo dello stato dell'applicazione Redux.
    
    *   Utilizzando useAppSelector con tipizzazione, si ottengono suggerimenti automatici e controllo del tipo durante la selezione dello stato nei componenti React.
    
    **4\. Vantaggi:**
    
    *   Gli hook personalizzati migliorano la leggibilità e la manutenibilità del codice.
    
    *   La tipizzazione aiuta a prevenire errori e migliora l'esperienza di sviluppo.

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,6 +1,8 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "./store";
 
+// Hook personalizzato per dispatch dell'azione
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
+// Hook personalizzato per la selezione dello stato con tipizzazione
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
