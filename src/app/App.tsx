dbe04c445065aa3beb5d309e85d3573a1779ad80
auritiwdesign <juancamilo.auriti@gmail.com>
2024-03-22 06:41:01 +0100

    installato redux

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { Store } from '@reduxjs/toolkit'
+
+import React from 'react'
+import { Provider } from'react-redux'
+
+const App = () => {
+    return (
+        <div>App</div>
+    )
+}
+
+export default App
